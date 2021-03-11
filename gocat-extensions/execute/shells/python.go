4c54119021a3c873a223021ba4b7c5fb10104341
Ivy Oeltjenbruns <ioeltjenbruns@mitre.org>
2021-03-11 17:02:57 -0500

    check python version and set executor shortName to pythonX for correct version

--- gocat-extensions/execute/shells/python.go
+++ gocat-extensions/execute/shells/python.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"runtime"
 	"fmt"
+	"strings"
 	"github.com/mitre/gocat/output"
 	"github.com/mitre/gocat/execute"
 )
@@ -17,24 +18,32 @@ type Python struct {
 }
 
 func init() {
-	if !(setPath("python3")) {
-		setPath("python")
-	}
+	setExecutor("python3")
+	setExecutor("python")
 }
 
-func setPath(name string) bool {
+func setExecutor(name string) bool {
 // Checks if python3 or python is available on the system and
-//sets the shell executor with the appropriate path
+// sets the shell executor with the appropriate path
 	var path string
-
+	var val string
+	var sName string
+	
 	if runtime.GOOS == "windows" {
 		path = name + ".exe"
 	} else {
 		path = name
 	}
 
+	if name == "python" {
+		val = checkVersion(name)
+		sName = "python" + val
+	} else {
+		sName = name
+	}
+
 	shell := &Python {
-		shortName: "python3",
+		shortName: sName,
 		path: path,
 		execArgs: []string{"-c"},
 	}
@@ -42,10 +51,24 @@ func setPath(name string) bool {
 		execute.Executors[shell.shortName] = shell
 		return true
 	} 
-	output.VerbosePrint(fmt.Sprintf("%s is not installed", name))
+	fmt.Print("%s is not installed", name)
 	return false
 }
 
+func checkVersion(name string) string {
+	var str_ver string
+// checks the python version 
+	version, err := exec.Command("python", "-c", "import platform; print(platform.python_version().split('.')[0])").CombinedOutput()
+	fmt.Print("version", string(version), "\n")
+	if err != nil {
+		fmt.Print("Error:", err, "\n")
+	}
+// returns the first number from the python version (i.e. python version 2.7.1 would return 2 as a string)
+	str_ver = string(version)
+	str_ver = strings.TrimSuffix(str_ver, "\n")
+	str_ver = strings.TrimSuffix(str_ver, "\r")
+	return str_ver
+}
 
 func (p *Python) Run(command string, timeout int, info execute.InstructionInfo) ([]byte, string, string) {
 	return runShellExecutor(*exec.Command(p.path, append(p.execArgs, command)...), timeout)
@@ -58,3 +81,7 @@ func (p *Python) String() string {
 func (p *Python) CheckIfAvailable() bool {
 	return checkExecutorInPath(p.path)
 } 
+
+func (p *Python) DownloadPayloadToMemory(payloadName string) bool {
+	return false
+}
